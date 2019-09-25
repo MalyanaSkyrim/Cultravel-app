5f8e2271ed4cfe64547b1e8905ba5ac8164d2b6b
Mohamed Malyana <malyanamohamed1@gmail.com>
2019-09-25 01:40:08 +0100

    Updates

---
+++ precache-manifest.229d26a9c6f01023f338c6a6b6ed6a41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a665c4ca64077382140dffabd1967a15",
+    "url": "/Cultravel-app/index.html"
+  },
+  {
+    "revision": "af83cfe3cec96b4e52c8",
+    "url": "/Cultravel-app/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "651538a67862cb2b6572",
+    "url": "/Cultravel-app/static/css/main.dbcb9fdc.chunk.css"
+  },
+  {
+    "revision": "af83cfe3cec96b4e52c8",
+    "url": "/Cultravel-app/static/js/2.8fe90fb5.chunk.js"
+  },
+  {
+    "revision": "651538a67862cb2b6572",
+    "url": "/Cultravel-app/static/js/main.cb0f6a63.chunk.js"
+  },
+  {
+    "revision": "45d3c8340ee40659fbeb",
+    "url": "/Cultravel-app/static/js/runtime~main.0f9792e4.js"
+  },
+  {
+    "revision": "c55e8a8e6f79467c0302ac2b2c46bd7b",
+    "url": "/Cultravel-app/static/media/morocco-ouarzazate.c55e8a8e.jpg"
+  },
+  {
+    "revision": "03574501c3c1334d9ea6c763e9247f6d",
+    "url": "/Cultravel-app/static/media/test-bg.03574501.jpg"
+  }
+]);
